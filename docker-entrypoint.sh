de104cb9d85d0e850389d99839039b87ebf9d0b7
leeco <leeco@leecodeMacBook-Pro-2.local>
2017-06-02 17:44:02 +0800

    second commit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 CONFIG="$JSTORM_HOME/conf/storm.yaml"
+##CONFIG="test.log"
+##if [ ! -f "$CONFIG" ]; then
+##    cat << EOF > "$CONFIG"
+##storm.zookeeper.servers: $1
+##if [ -n $3 ]; then
+  
+##fi
+##nimbus.host: $2
+##EOF
+##fi
 
-if [ ! -f "$CONFIG" ]; then
-    cat << EOF > "$CONFIG"
-storm.zookeeper.servers: [zookeeper]
-nimbus.seeds: [nimbus]
-EOF
-fi
-
-
-exec "$@"
+echo "storm.zookeeper.servers:" $1 >> "$CONFIG"
+echo "nimbus.host:" $2 >> "$CONFIG"
+#echo $3
+#exec "$3"
